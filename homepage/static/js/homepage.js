313629c583e385e8c14dc8e5de2eefc2d920d41f
snackattas <zach.attas@gmail.com>
2017-01-29 16:40:08 -0600

    step in the right direction

--- homepage/static/js/homepage.js
+++ homepage/static/js/homepage.js
@@ -1,29 +1,30 @@
 $( document ).ready( function () {
-
+// small bit of javascript to enable smooth scrolling when header buttons are clicked
   function scroller (event) {
     var scroll_to = event.data.scroll_to
     $('html,body').animate({
       scrollTop: $(scroll_to).offset().top
     }, 1000);
   }
-
   var menu_links = $(".button-link");
-
   for (i = 0; i < menu_links.length; i++) {
     var scroll_from = $(menu_links[i])
     var scroll_to = "#" + $(scroll_from).data("id")
     $(scroll_from).on("click", {scroll_to: scroll_to}, scroller)
   };
 
+// google analytics insert
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
   ga('create', 'UA-90308284-1', 'auto');
   ga('send', 'pageview');
+// end google analytics
 });
-
+// Grab headervideo html from url and load it when everything else is loaded on the page
 window.onload = function() {
-  $('.header-video').append("<video autoplay='' class='header-video__media' loop=''><source src='static/video/coding-smaller.mp4' type='video/mp4'><source src='static/video/coding.ogv' type='video/ogv'/><source src='static/video/coding.webm' type='video/webm'/>Sorry, your browser doesn't support HTML5 video.</video>");
+  $.get("headerVideo", function (data) {
+    $('.header-video').append(data);
+  });
 };
--- homepage/views.py
+++ homepage/views.py
@@ -6,6 +6,67 @@ from django.core.mail import send_mail
 import logging
 import os
 
+
+def homepage(request):
+    """
+    Main endpoint.  Renders homepage with contact form.
+    """
+    message = None
+    if request.POST:
+        message = helper_send_email(request.POST)
+    return render(request, 'homepage.html',
+        {'menus':  helper_create_menu_tuple(), 'form': ContactForm(),
+        'message': message})
+
+def headerVideo(request):
+    """
+    Simple endpoint to render html content for header video
+    """
+    return render(request, 'headerVideo.html')
+
+
+def helper_send_email(data):
+    """
+    Sends email and returns message determining whether email went through on our end (doesn't validate email address, bad emails will fail elsewhere)
+    """
+    # Define messagetuple to send back to client
+    Message = namedtuple("Message", ["message", "error"])
+    # Default success message
+    message = Message(
+        message="Contact form submitted successfully",
+        error=False)
+
+    # Reason for contacting transformed to email title
+    reason_for_contacting = data.get('reason_for_contacting')
+    if not reason_for_contacting:
+        reason_for_contacting = "Contact Form filled out on zattas.me"
+
+    # Determine who emails are sent to
+    recipients = [os.environ.get('HOST_EMAIL')]
+    email_body = data.get('message')
+    email = data.get('email')
+    send_to_yourself = data.get('send_to_yourself')
+    if send_to_yourself:
+        recipients.append(email)
+    else:
+        # If user doesn't want a copy sent to themselves, add their email to the email body
+        add_email_to_message = "Email: %s\n\n" % email
+        email_body = add_email_to_message + email_body
+
+    try:
+        send_mail(
+            subject=reason_for_contacting,
+            message=email_body,
+            from_email=os.environ.get('HOST_EMAIL'),
+            to_email=recipients,
+            fail_silently=False)
+    except:
+        message = Message(
+            message="Error submitting contact form.  Are you sure your "\
+            "email is valid?",
+            error=True)
+    return message
+
 def helper_create_menu_tuple():
     CustomMenu = namedtuple("CustomMenu",
         ["fancy_number", "menu_name", "menu_name_without_spaces"])
@@ -17,41 +78,3 @@ def helper_create_menu_tuple():
             menu,
             menu.replace(" ","")))
     return menu_tuples
-
-def homepage(request):
-    message = None
-    if request.POST:
-        message = helper_send_email(request.POST)
-    return render(request, 'homepage.html',
-        {'menus':  helper_create_menu_tuple(), 'form': ContactForm(),
-        'message': message})
-
-def helper_send_email(data):
-    Message = namedtuple("Message", ["message", "error"])
-    message = Message("Contact form submitted successfully", False)
-
-    email = data.get('email')
-    reason_for_contacting = data.get('reason_for_contacting')
-    email_body = data.get('message')
-    send_to_yourself = data.get('send_to_yourself')
-
-    if not reason_for_contacting:
-        reason_for_contacting = "Contact Form filled out on zattas.me"
-
-    recipients = [os.environ.get('HOST_EMAIL')]
-    if send_to_yourself:
-        recipients.append(email)
-    else:
-        add_email_to_message = "Email: %s\n\n" % email
-        email_body = add_email_to_message + email_body
-    try:
-        send_mail(
-            reason_for_contacting,
-            email_body,
-            os.environ.get('HOST_EMAIL'),
-            recipients,
-            fail_silently=False)
-    except:
-        message = Message("Error submitting contact form.  Are you sure your "\
-            "email is valid?", True)
-    return message
