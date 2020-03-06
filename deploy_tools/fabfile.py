6a5fdfd7b63784ba04a275b01aa88487cb9266fb
snackattas <zach.attas@gmail.com>
2020-03-05 23:11:05 -0600

    semicolon instead of newline

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -39,8 +39,8 @@ def _update_settings(source_folder, site_name):
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
-        append(secret_key_file, "DJANGO_KEY = '%s'" % (key,))
-    append(settings_path, '\nfrom .secret_key import DJANGO_KEY')
+        append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
+    append(settings_path, 'from .secret_key import SECRET_KEY;')
 
 
 def _update_virtualenv(source_folder):
