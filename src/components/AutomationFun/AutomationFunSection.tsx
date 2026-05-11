936ac7b7fd0977f30de293512ec9851c0c5b4190
snackattas <zach.attas@gmail.com>
2026-05-10 21:02:43 -0500

    run -> job

--- src/components/AutomationFun/AutomationFunSection.tsx
+++ src/components/AutomationFun/AutomationFunSection.tsx
@@ -527,7 +527,7 @@ const handleCmdCopy = (text: string, index: number) => {
             className={styles["ciTooltip"]}
             style={{ left: ciTooltipPos.x + 14, top: ciTooltipPos.y + 14 }}
           >
-            {ciGridHovered && <><span className={styles["ciTooltipClick"]}>click any row to open that run in github</span><br /></>}
+            {ciGridHovered && <><span className={styles["ciTooltipClick"]}>click any row to open that job in github</span><br /></>}
             every deploy triggers a full matrix of tests, verifying that each combo of automation tool and language correctly trips the detection logic, isn&apos;t mistaken for a human, and claims the AMAZING reward
           </div>,
           document.body
